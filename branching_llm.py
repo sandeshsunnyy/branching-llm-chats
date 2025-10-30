0b792dafee10e8aa092e091160e3d7d5080da20b
sandesh <sandeshsunny2020@gmail.com>
2025-10-30 21:21:39 +0530

    A seperate file for utilities were added and the prompts have been moved to that file. Also the branch_chats table as well as indexing on parent_id is also done.

--- branching_llm.py
+++ branching_llm.py
@@ -9,6 +9,7 @@ from langgraph.graph.message import add_messages
 from typing_extensions import Annotated, TypedDict
 from datetime import datetime, timezone
 from langgraph.checkpoint.base import Checkpoint, CheckpointMetadata
+from utilities import prompt_template, summarizer_prompt_template
 try: 
   from dotenv import load_dotenv
 
@@ -20,45 +21,6 @@ except ImportError:
 
 model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 memory = MemorySaver()
-
-prompt_template = ChatPromptTemplate.from_messages([
-    (
-        "system",
-        "You talk like a 15th century man. Answer questions accordingly in {language}"
-    ),
-    MessagesPlaceholder(variable_name="messages"),
-])
-
-summarizer_prompt_template = ChatPromptTemplate.from_messages([
-   (
-      "system",
-      """
-         Persona:
-         You are a summarizer. Your job is to summarize the messages given to you. A context will also be given so that you understand the whole picture. So summarize the text given under 'Messages to summarize'.
-
-         Actual Workflow:
-         I am building a project where people can branch from current chats to have different threads of conversations so that the main branch is not subjected to context decay. But if i want to return to the main branch, the summary of the branch must also be included so as to make the main context less oblivious. So your job is to give me that summary.
-
-         Output Format:
-         The result should be just the summary as a string. If the conversation is not worth having a summary, just reply what you thought his/her intention was.
-
-         Final Instructions:
-         1. Stick strictly to the output format.
-         2. In the context provided, the AIMessage might have a persona. So it would be good if you could summarize based on that persona itself. I might have to deduct what the persona is yourself. If you are not able to, then it's fine just return a normal string.
-
-         Context is given below:
-      """
-   ),
-   MessagesPlaceholder(variable_name="context"),
-   (
-      "human",
-      """
-         Messages to summarize:
-
-         {messages_to_summarize}
-      """
-   ),
-])
 
 trimmer = trim_messages(
     max_tokens=10000,
---
+++ utilities.py
@@ -0,0 +1,40 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+prompt_template = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "You talk like a 15th century man. Answer questions accordingly in {language}"
+    ),
+    MessagesPlaceholder(variable_name="messages"),
+])
+
+summarizer_prompt_template = ChatPromptTemplate.from_messages([
+   (
+      "system",
+      """
+         Persona:
+         You are a summarizer. Your job is to summarize the messages given to you. A context will also be given so that you understand the whole picture. So summarize the text given under 'Messages to summarize'.
+
+         Actual Workflow:
+         I am building a project where people can branch from current chats to have different threads of conversations so that the main branch is not subjected to context decay. But if i want to return to the main branch, the summary of the branch must also be included so as to make the main context less oblivious. So your job is to give me that summary.
+
+         Output Format:
+         The result should be just the summary as a string. If the conversation is not worth having a summary, just reply what you thought his/her intention was.
+
+         Final Instructions:
+         1. Stick strictly to the output format.
+         2. In the context provided, the AIMessage might have a persona. So it would be good if you could summarize based on that persona itself. I might have to deduct what the persona is yourself. If you are not able to, then it's fine just return a normal string.
+
+         Context is given below:
+      """
+   ),
+   MessagesPlaceholder(variable_name="context"),
+   (
+      "human",
+      """
+         Messages to summarize:
+
+         {messages_to_summarize}
+      """
+   ),
+])
